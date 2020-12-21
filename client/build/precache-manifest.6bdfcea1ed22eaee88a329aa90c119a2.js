74669ef183019417bc1445cd6afecad295b11b49
Uma Jain <umajain1000@gmail.com>
2020-12-21 11:49:08 +0530

    build folder removed from github

---
+++ client/build/precache-manifest.6bdfcea1ed22eaee88a329aa90c119a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd270febd262f601ad1881593d9abf4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4d6405958da84e987be",
+    "url": "/static/css/main.9bfb2240.chunk.css"
+  },
+  {
+    "revision": "d6e41bd6a5fb3da85dd3",
+    "url": "/static/js/2.5876d056.chunk.js"
+  },
+  {
+    "revision": "6c6c2f0dbf5da04265b3a75d00f44e52",
+    "url": "/static/js/2.5876d056.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4d6405958da84e987be",
+    "url": "/static/js/main.6c5ca74b.chunk.js"
+  },
+  {
+    "revision": "636593fd05e15a7bf904",
+    "url": "/static/js/runtime-main.ece9d328.js"
+  }
+]);
